51df0275972c6cd1aabf6989de6e9a5e9c79fdea
matanki.saito <matanki.saito@gmail.com>
2022-02-23 14:58:58 +0900

    SMAAAAASH!

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 import spacy
 
 nlp = spacy.load('ja_ginza')
-doc = nlp(u'銀座でランチをご一緒しましょう。')
+doc = nlp('銀座でランチをご一緒しましょう。')
 for sent in doc.sents:
     for token in sent:
-        print(
-            token.i,
-            token.orth_,
-            token.lemma_,
-            token.norm_,
-            token.morph.get("Reading"),
-            token.pos_,
-            token.morph.get("Inflection"),
-            token.tag_,
-            token.dep_,
-            token.head.i,
-        )
-    print(u'EOS')
+        print(token.i)
+    print('EOS')
 
