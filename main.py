3d54df8ee8f77100f49aa6dabecb92972a8410e7
matanki.saito <matanki.saito@gmail.com>
2022-02-23 15:06:26 +0900

    a

--- main.py
+++ main.py
@@ -1,4 +1,42 @@
 import spacy
+import sys
+import os
+
+# https://note.com/npaka/n/n6fa359ac611c
+# システム辞書のパス
+sys_dic_path = ''
+for path in sys.path:
+    path = path+'/sudachidict_core/resources/system.dic'
+    if os.path.exists(path):
+        sys_dic_path = path
+        break
+print('sys_dic_path:', sys_dic_path)
+
+# ユーザー辞書のパス
+user_dic_path = os.getcwd()+'/user.dic'
+print('user_dic_path:', user_dic_path)
+
+# sudahci.jsonのパス
+sudachi_path = None
+for path in sys.path:
+    path = path+'/sudachipy/resources/sudachi.json'
+    if os.path.exists(path):
+        sudachi_path = path
+        break
+print('sudachi_path:', sudachi_path)
+
+# userDictの追加
+if sudachi_path != None:
+    lines = []
+    with open(sudachi_path) as f:
+        lines = f.readlines()
+    for i in range(len(lines)-1):
+        if lines[i].find('"characterDefinitionFile"') >= 0 and lines[i+1].find('"userDict"') < 0:
+            lines.insert(i+1,'    "userDict" : ["'+user_dic_path+'"],\n')
+            with open(sudachi_path, 'w', encoding='utf-8', newline='\n') as f:
+                f.writelines(lines)
+            print('update')
+            break
 
 nlp = spacy.load('ja_ginza')
 doc = nlp('銀座でランチをご一緒しましょう。')
